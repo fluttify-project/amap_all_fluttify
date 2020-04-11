b816c134193a1137545923fa7a13f471aa643ee0
yohom <yohombao@qq.com>
2020-04-11 17:30:25 +0800

    enhance: 附近 初步

--- example/lib/demo/demo.screen.dart
+++ example/lib/demo/demo.screen.dart
@@ -1,9 +1,7 @@
-import 'package:decorated_flutter/decorated_flutter.dart';
 import 'package:demo_widgets/demo_widgets.dart';
 import 'package:flutter/material.dart';
 
-import 'advance/nearby.screen.dart';
-import 'basic/show_search_result.screen.dart';
+import 'nearby.screen.dart';
 
 class MapDemo extends StatelessWidget {
   @override
@@ -11,21 +9,10 @@ class MapDemo extends StatelessWidget {
     return ListView(
       children: <Widget>[
         FunctionGroup(
-          headLabel: '基础',
+          headLabel: '附近',
           children: <Widget>[
             FunctionItem(
-              label: '显示路线规划结果',
-              sublabel: 'ShowSearchResultScreen',
-              target: ShowSearchResultScreen(),
-            ),
-          ],
-        ),
-        SPACE_BIG_VERTICAL,
-        FunctionGroup(
-          headLabel: '高阶',
-          children: <Widget>[
-            FunctionItem(
-              label: '附近',
+              label: '显示附近',
               sublabel: 'NearbyScreen',
               target: NearbyScreen(),
             ),
